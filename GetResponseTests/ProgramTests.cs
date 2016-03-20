cf8b37a32dff09b4967b2f8b9255c663516ac0e2
beata.jakimiak.rychlicka@gmail.com <beata.jakimiak.rychlicka@gmail.com>
2016-03-20 11:14:14 +0100

    Prepared project

---
+++ GetResponseTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetResponse.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
